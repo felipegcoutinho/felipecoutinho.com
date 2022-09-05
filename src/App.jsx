0fc7daa744048aa414bd04e583b7bbbfe7f676e4
Felipe Coutinho <felipi@live.com>
2022-09-05 10:26:41 -0300

    add jsx files from old project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+import Bio from './bio.jsx';
+import Project from './project.jsx';
+import Aboutme from './aboutme.jsx';
+
+function App() {
+  return (
+    <div>
+      <Bio />
+      <Project />
+      <Aboutme />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/project.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function Project() {
+  return (
+    <div id="projects-container">
+      <h1>Project</h1>
+    </div>
+  );
+}
+
+export default Project;
