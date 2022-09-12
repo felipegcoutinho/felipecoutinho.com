8f310f669d702be85e0116ef0ce4e038d627d9e0
Felipe Coutinho <felipi@live.com>
2022-09-12 09:50:53 -0300

    update

--- src/Components/aboutme.jsx
+++ src/Components/aboutme.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './AboutMe.module.css'
+import style from './About.module.css';
 
 function Aboutme() {
   return (
@@ -7,16 +7,14 @@ function Aboutme() {
       <div className={style.description}>
         <h1>About Me</h1>
         <p>
-
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at
           neque in tellus fringilla viverra vel quis nibh. Sed ac elit et mauris
           lacinia dictum. In cursus neque non semper aliquet. Phasellus eget
           consequat tellus, sit amet luctus risus. Nulla consectetur sit amet
           leo in placerat.
         </p>
-
       </div>
-    </main >
+    </main>
   );
 }
 
