f92703a95df867685bd13981f6a8fa2b86a7b0de
Felipe Coutinho <felipi@live.com>
2022-09-19 11:17:06 -0300

    colors tests

--- src/Components/project.jsx
+++ src/Components/project.jsx
@@ -7,13 +7,11 @@ function Project() {
     <main className={style.container}>
       <div className={style.gridContainer}>
 
-        <a href="">
-          <div className={stylec.cards}>
-            <header><h1>Portifólio</h1></header>
-            <summary><h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3></summary>
-            <footer><h3>#JS #HTML</h3></footer>
-          </div>
-        </a>
+        <div className={stylec.cards}>
+          <header><h1><a href="#">Portifólio</a></h1></header>
+          <summary><h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3></summary>
+          <footer><h3>#JS #HTML #CSS </h3></footer>
+        </div>
 
       </div>
     </main>
