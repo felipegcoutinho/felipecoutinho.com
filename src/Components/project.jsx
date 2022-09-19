e4dbfb73acfdd36d67dc615169949a27d002fc85
Felipe Coutinho <felipi@live.com>
2022-09-19 10:04:33 -0300

    update

--- src/Components/project.jsx
+++ src/Components/project.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
 import style from './Project.module.css'
+import stylec from "./CardsProject.module.css";
 
 function Project() {
   return (
     <main className={style.container}>
       <div className={style.gridContainer}>
-        <div className={style.cards}>
-          <header><h1>Titulo</h1></header>
-          <article><h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3></article>
-          <footer><h3>Tags</h3></footer>
-        </div>
 
-
-
-
-
-
-
-
-
-
-        <div className={style.cards}>Card 2</div>
-        <div className={style.cards}>Card 3</div>
-        <div className={style.cards}>Card 4</div>
-
+        <a href="">
+          <div className={stylec.cards}>
+            <header><h1>Portifólio</h1></header>
+            <summary><h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3></summary>
+            <footer><h3>#JS #HTML</h3></footer>
+          </div>
+        </a>
 
       </div>
     </main>
