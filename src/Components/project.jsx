20c76cbabb229441da9c58b6f6032885d43357f8
Felipe Coutinho <felipi@live.com>
2022-09-21 11:54:25 -0300

    update colors

--- src/Components/project.jsx
+++ src/Components/project.jsx
@@ -6,31 +6,49 @@ function Project() {
   return (
     <main className={style.container}>
       <div className={style.gridContainer}>
-
-        <div className={`${stylec.cards} ${stylec.blue}`}>
-          < header > <h1><a href="#">Portifólio</a></h1></header>
-          <summary><h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3></summary>
-          <footer><h3>#JS #HTML #CSS </h3></footer>
-        </div>
-
-        <div className={`${stylec.cards} ${stylec.yellow}`}>
-          < header > <h1><a href="#">Portifólio</a></h1></header>
-          <summary><h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3></summary>
-          <footer><h3>#JS #HTML #CSS </h3></footer>
-        </div>
-
-        <div className={`${stylec.cards} ${stylec.x}`}>
-          < header > <h1><a href="#">Portifólio</a></h1></header>
-          <summary><h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3></summary>
-          <footer><h3>#JS #HTML #CSS </h3></footer>
-        </div>
-
-
-
+        <Card
+          url="#"
+          header="Portifólio"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          tags="JS HTML CSS"
+        />
+        <Card
+          url="#"
+          header="Portifólio"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          tags="JS HTML CSS"
+        />
+        <Card
+          url="#"
+          header="Portifólio"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          tags="JS HTML CSS"
+        />
 
       </div >
     </main >
   );
 }
 
+
+
+function Card(props) {
+  return (
+    <div className={`${stylec.cards}`}>
+      <header><h1><a href={props.url}>{props.header}</a></h1></header>
+      <summary><h3>{props.desc}</h3></summary>
+      <footer><h3>{props.tags}</h3></footer>
+    </div>
+
+  )
+}
+
+
 export default Project;
+
+
+{/* <div className={`${stylec.cards} ${stylec.yellow}`}>
+< header > <h1><a href="#">Portifólio</a></h1></header>
+<summary><h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3></summary>
+<footer><h3>#JS #HTML #CSS </h3></footer>
+</div> */}
