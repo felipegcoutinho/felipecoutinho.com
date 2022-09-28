36b17fcaa2396ddeb8af378a16c34f00cba8bbf8
Felipe Coutinho <felipi@live.com>
2022-09-28 11:55:05 -0300

    update

--- src/Components/About/StackCarrousel.jsx
+++ src/Components/About/StackCarrousel.jsx
@@ -2,12 +2,59 @@ import React from 'react'
 import style from "./StackCarrousel.module.css";
 
 function StackCarrousel() {
+  // return (
+  //   // <div class={style.slider}>
+  //   //   <div class={style.slidetrack}>
+  //   //     <div class={style.slide}>
+  //   //       <img
+  //   //         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+  //   //         height="100"
+  //   //         width="250"
+  //   //         alt=""
+  //   //       />
+  //   //     </div>
+  //   //     <div class={style.slide}>
+  //   //       <img
+  //   //         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+  //   //         height="100"
+  //   //         width="250"
+  //   //         alt=""
+  //   //       />
+  //   //     </div>
+  //   //     <div class={style.slide}>
+  //   //       <img
+  //   //         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+  //   //         height="100"
+  //   //         width="250"
+  //   //         alt=""
+  //   //       />
+  //   //     </div>
+  //   //     <div class={style.slide}>
+  //   //       <img
+  //   //         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+  //   //         height="100"
+  //   //         width="250"
+  //   //         alt=""
+  //   //       />
+  //   //     </div>
+  //   //   </div>
+  //   // </div>
+
+  //   <span></span>
+
+
   return (
     <div className={style.stackcarrousel}>
       <div className={style.stacklogos}>REACT - JS - CSS</div>
     </div>
   )
+
+
 }
+
+
+
+
 
 function SVG() {
   return (
